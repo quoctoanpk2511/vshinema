4517e45791f6cd2dddece0db2df4c40cd40a5985
quoctoanpk2511 <quoctoanpk2511@gmail.com>
2022-05-23 22:59:38 +0700

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="layoutDefault__nav navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Shinema</a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/"
+              >Home</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">TV Shows</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">Movies</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about"
+              >New & Popular</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">My List</router-link>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
