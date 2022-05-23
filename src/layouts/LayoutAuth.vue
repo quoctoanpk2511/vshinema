089d67ce06ab0bf10ab9fdfb86ba0a2e44500653
quoctoanpk2511 <quoctoanpk2511@gmail.com>
2022-05-23 23:01:35 +0700

    create layout

---
+++ src/layouts/LayoutAuth.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="layoutAuth">
+    <img class="logo" src="../statics/images/shinema-logo.png" />
+    <slot />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "LayoutAuth",
+  components: {
+    Footer,
+  },
+};
+</script>
+
+<style>
+.layoutAuth {
+  height: 100vh;
+  background-image: url("@/statics/images/layoutauth-bg.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+.logo {
+  height: 80px;
+  padding-left: 20px;
+  position: absolute;
+}
+</style>
---
+++ src/layouts/LayoutDefault.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layoutDefault">
+    <navbar></navbar>
+    <slot />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "LayoutDefault",
+  components: { Navbar, Footer },
+};
+</script>
