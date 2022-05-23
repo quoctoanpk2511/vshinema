df936238f66f6658a7c20fa50e9f9bcc64b365a1
quoctoanpk2511 <quoctoanpk2511@gmail.com>
2022-05-23 23:02:36 +0700

    add home page

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+import LayoutDefault from "../layouts/LayoutDefault.vue";
+
+export default {
+  name: "Home",
+  created() {
+    this.$emit("update:layout", LayoutDefault);
+  },
+};
+</script>
