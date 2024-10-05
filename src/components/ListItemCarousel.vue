45626ae3965fa0fc66da43e44f968dd4d95660c9
toanpq <quoctoanpk2511@gmail.com>
2024-10-05 20:44:45 +0900

    Add carrd transform css

--- src/components/ListItemCarousel.vue
+++ src/components/ListItemCarousel.vue
@@ -1,12 +1,12 @@
 <template>
-  <b-row class="pt-5">
+  <b-row class="pt-4">
     <b-col>
       <div class="list-item_title pb-2">{{ title }}</div>
-      <b-carousel id="carousel-1" :interval="4000" controls>
+      <b-carousel id="carousel-1" :interval="94000" controls>
         <b-carousel-slide v-for="group in carouselMovies" :key="group.id">
           <template v-slot:img>
-            <b-row>
-              <b-col cols="12" md="2" v-for="item in group" :key="item.id">
+            <b-row class="m-0">
+              <b-col class="card-content_img" cols="12" md="2" v-for="item in group" :key="item.id">
                 <div>
                   <b-card-img :src="imgUrl + item.backdrop_path" alt=" "></b-card-img>
                 </div>
@@ -43,5 +43,16 @@ export default {
 .carousel-control-next {
   right: -55px !important;
   width: 5% !important;
+}
+
+.card-content_img {
+  cursor: pointer;
+  padding: 30px;
+  transition: transform .2s; /* Animation */
+  margin: 0 auto;
+}
+
+.card-content_img:hover {
+  transform: scale(1.2);
 }
 </style>
